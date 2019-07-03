9d31b61abf111eebfc9e9833088d5a11edf48bd2
prabhu-durasoft <iamprabhu@yahoo.com>
2019-07-03 10:36:39 +0530

    Day03

--- Day03/src/ParallelStreamExample1.java
+++ Day03/src/ParallelStreamExample1.java
@@ -1,22 +1,50 @@
+import java.util.Arrays;
 import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 public class ParallelStreamExample1 {
 
 	public static void main(String[] args) {
-		compute();
+		int numberOfCores = Runtime.getRuntime().availableProcessors();
+		System.out.println(numberOfCores);
+		//computeFirstEven();
 		System.out.println("End of main");
+		Stream<Integer> stream = createStream();
+		stream
+			.filter(e -> e % 2 == 0)
+			.sequential()
+			.forEach(System.out::println);
+	}
+	
+	private static Stream<Integer> createStream() {
+		return Arrays.asList(1, 2, 3, 4, 5).stream().parallel();
 	}
 	
 	private static boolean findEven(int number) {
-		System.out.println("filter: " + number);
+		System.out.println("filter: " + number + " (" + Thread.currentThread() + ")");
 		sleep(1);
 		return number % 2 == 0;
+	}
+	
+	private static void computeFirstEven() {
+		IntStream numbers = IntStream.range(1, 101);
+		int even = numbers
+			.filter(ParallelStreamExample1::findEven)
+			//.parallel()
+			.filter(e -> true)
+			//.sequential()
+			.filter(e -> true)
+			.parallel()
+			.findFirst()
+			.getAsInt();
+		System.out.println("First Even: " + even);
 	}
 	
 	private static void compute() {
 		IntStream numbers = IntStream.range(1, 101);
 		numbers
 			.filter(ParallelStreamExample1::findEven)
+			.parallel()
 			.forEach(e -> {});
 	}
 
